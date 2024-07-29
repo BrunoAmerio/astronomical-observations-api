abbaed96ee17d15205edd7701c7dd369d257df32
BrunoAmerio <brunoamerio00@gmai.com>
2024-07-29 12:54:05 -0300

    fix: delete console.log

--- server/middlewares/checkRole.js
+++ server/middlewares/checkRole.js
@@ -1,7 +1,5 @@
 function checkRole(requiredRole) {
   return (req, res, next) => {
-    console.log('Estoy aqui')
-
     if (req.auth.role !== requiredRole) {
       return res.status(403).json({ error: 'Forbidden: Insufficient role' })
     }
